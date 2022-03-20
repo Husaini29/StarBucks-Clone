0b36f3e7275b2bbe257732f19eeeee552e115c8f
Husaini29 <Husainibohra86@gmail.com>
2022-03-20 17:01:49 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function mouseOverCoffee(){
+    document.getElementById('coffeeDropDown').style.visibility="visible"
+}
+
+function mouseOutCoffee(){
+    document.getElementById('coffeeDropDown').style.visibility="hidden"
+}
+
+function mouseOverMenu(){
+    document.getElementById('menuDropDown').style.visibility="visible"
+}
+
+function mouseOutMenu(){
+    document.getElementById('menuDropDown').style.visibility="hidden"
+}
+
+function mouseOverResponsibility(){
+    document.getElementById('responsibilityDropDown').style.visibility="visible"
+}
+
+function mouseOutResponsibility(){
+    document.getElementById('responsibilityDropDown').style.visibility="hidden"
+}
+
+function mouseOverCoffeeHouse(){
+    document.getElementById('coffeeHouseDropDown').style.visibility="visible"
+}
+
+function mouseOutCoffeeHouse(){
+    document.getElementById('coffeeHouseDropDown').style.visibility="hidden"
+}
+
+function mouseOverAboutUs(){
+    document.getElementById('aboutUsDropDown').style.visibility="visible"
+}
+
+function mouseOutAboutUs(){
+    document.getElementById('aboutUsDropDown').style.visibility="hidden"
+}
